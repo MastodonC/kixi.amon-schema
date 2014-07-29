1e8ec3fa87577d7484c97e056b0be3faca2fe809
Max Droog Hayes <max@mastodonc.com>
2014-07-29 11:53:22 +0100

    Bump version number.

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject kixi/amon-schema "0.1.0-SNAPSHOT"
+(defproject kixi/amon-schema "0.1.1-SNAPSHOT"
   :description "A Prismatic Schema for AMON v4 requests and responses."
   :url "http://github.com/MastodonC/kixi.amon-schema"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[prismatic/schema "0.2.1"]
                  [schema-contrib "0.1.3"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+
+  :scm {:name "git"
+        :url "git@github.com:MastodonC/kixi.amon-schema.git"})
