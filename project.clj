5948a9e431d47c2d6a635457454f7417a1a191d2
Max Droog Hayes <max@mastodonc.com>
2014-08-07 14:53:36 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/MastodonC/kixi.amon-schema"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[prismatic/schema "0.2.4"]
+  :dependencies [[prismatic/schema "0.2.6"]
                  [schema-contrib "0.1.3"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :scm {:name "git"
