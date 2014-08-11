d4ceb36b102804e864e01a5ca2782024d1f1c3ab
Max Droog Hayes <max@mastodonc.com>
2014-08-11 13:03:26 +0100

    Bump version for release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kixi/amon-schema "0.1.3-SNAPSHOT"
+(defproject kixi/amon-schema "0.1.3"
   :description "A Prismatic Schema for AMON v4 requests and responses."
   :url "http://github.com/MastodonC/kixi.amon-schema"
   :license {:name "Eclipse Public License"
